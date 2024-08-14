15707274e2391ec4f8396ee29332571b96643c14
Sanjay Singh <sanjaysinghg2u@gmail.com>
2024-08-14 14:22:25 +0530

    add productbyid

--- src/main/java/com/sanjay/ecom/controller/ProductController.java
+++ src/main/java/com/sanjay/ecom/controller/ProductController.java
@@ -2,8 +2,13 @@ package com.sanjay.ecom.controller;
 
 import com.sanjay.ecom.model.Product;
 import com.sanjay.ecom.service.ProductService;
+import org.apache.catalina.connector.Response;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,8 +28,19 @@ public class ProductController {
     }
 
     @GetMapping("/products")
-    public List<Product> getProducts()
+    public ResponseEntity<List<Product>> getProducts()
     {
-        return service.getProducts();
+        return new ResponseEntity<>(service.getProducts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/product/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable int id)
+    {
+        Product product = service.getProductById(id);
+
+        if(product != null)
+            return new ResponseEntity<>(product, HttpStatus.OK);
+        else
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
--- src/main/java/com/sanjay/ecom/service/ProductService.java
+++ src/main/java/com/sanjay/ecom/service/ProductService.java
@@ -16,4 +16,8 @@ public class ProductService {
     public List<Product> getProducts() {
         return repo.findAll();
     }
+
+    public Product getProductById(int id) {
+        return repo.findById(id).orElse(null);
+    }
 }
