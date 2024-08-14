55a130e10ac56b4c405567b0f6fa954efeb0cf4c
Sanjay Singh <sanjaysinghg2u@gmail.com>
2024-08-14 13:46:33 +0530

    create product repo

---
+++ src/main/java/com/sanjay/ecom/repo/ProductRepo.java
@@ -0,0 +1,10 @@
+package com.sanjay.ecom.repo;
+
+import com.sanjay.ecom.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductRepo extends JpaRepository<Product, Integer> {
+
+}
